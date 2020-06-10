2190d762265d27bd0888633a6f85d5eb25f603fc
Larry_A <larryagbana@gmail.com>
2020-06-10 15:25:54 -0400

    handle route paths

--- server.js
+++ server.js
@@ -4,29 +4,42 @@ const compression = require('compression')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
+const path = require('path')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.use(compression())
 app.use(morgan('dev'))
+app.use(cors())
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutTracker', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutTracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }
+)
+
+// Use api workouts route with api.js route handler
+app.use('api/workouts', require('./routes/api.js'))
+
+// Display the exercise.html page
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/exercise.html'))
 })
 
-// routes
+// Display stats html page
+app.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/stats.html'))
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`App is running on PORT ${PORT}`)
+  console.log(`App is running on PORT ${PORT}`)
 })
-
-
