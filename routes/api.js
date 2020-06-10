880d522c259cd7fc20ffb62ed371fedc73b886a9
Larry_A <larryagbana@gmail.com>
2020-06-10 10:04:55 -0400

    route handler for rendering all current workouts on the page

---
+++ routes/api.js
@@ -0,0 +1,37 @@
+const router = require('express').Router()
+const Workout = require('../models/Workout')
+
+router.post('/api/workout', ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    })
+})
+
+router.post('/api/workout/bulk', ({ body }, res) => {
+  Workout.insertMany(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    })
+})
+
+// Render page with all currently posted workouts
+// Sort the workouts by date in descending order
+router.get('/api/workouts', (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    })
+})
+
+module.exports = router
