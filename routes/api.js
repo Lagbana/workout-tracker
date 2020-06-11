60b215cd46e0c8d7da5a00d26d4a67b9baec59e1
Larry_A <larryagbana@gmail.com>
2020-06-11 18:14:08 -0400

    update workout route handler

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,9 @@
 const router = require('express').Router()
 const { Workout } = require('../models')
 
+// UPDATE a workout
+// Use a workout's id to find it
+// Push the user input in the request body into the database
 router.put('/:id', async (req, res) => {
   try {
     const updatedWorkout = await Workout.findOneAndUpdate(
@@ -15,39 +18,6 @@ router.put('/:id', async (req, res) => {
   }
 })
 
-// POST a new workout
-// Create a new instance of workout
-// Save it, then send the new workout back to the client
-router.post('/', (req, res, next) => {
-  const newWorkout = new Workout(req.body)
-  newWorkout
-    .save()
-    .then(dbWorkout => res.status(201).json(dbWorkout))
-    .catch(next)
-})
 
-// Render page with all currently posted workouts
-// Sort the workouts by date in descending order
-router.get('/', async (req, res) => {
-  try {
-    const workouts = await Workout.find({})
-    // console.log(workouts)
-    workouts.forEach(workout => {
-      let totalTime = 0
-      workout.exercises.forEach(exercise => {
-        totalTime += exercise.duration
-      })
-      workout.totalDuration = totalTime
-    })
-
-    res.json(workouts)
-  } catch (err) {
-    res.status(400).json(err)
-  }
-})
-
-router.get('/range', (req, res) => {
-  res.status(307).redirect('/api/workouts')
-})
 
 module.exports = router
