0e9ce45f2450a85c275186f07b074a8b17855134
Larry_A <larryagbana@gmail.com>
2020-06-11 23:29:05 -0400

    code refactor and comments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Import app dependencies
 const express = require('express')
 const mongoose = require('mongoose')
 const compression = require('compression')
@@ -6,16 +7,19 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
 
+// Set up Express app
 const app = express()
 
+// Set up the Express app for handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-// app.use(compression())
+app.use(compression())
 app.use(logger('dev'))
-// app.use(cors())
+app.use(cors())
 
+// Setup connection to mongoDB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -31,11 +35,12 @@ app.get('/exercise', (req, res) => {
   res.sendFile(path.join(__dirname, './public/exercise.html'))
 })
 
-// Display stats html page
+// Display stats.html page
 app.get('/stats', (req, res) => {
   res.sendFile(path.join(__dirname, './public/stats.html'))
 })
 
+// Listen on specified PORT and start the express app
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`App is running on PORT ${PORT}`)
